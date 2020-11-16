90edaeccf964ab0185c95ef7e42c561b07355fc6
yaacine <gy_zidelmal@esi.dz>
2020-11-16 11:28:27 +0100

    chore: added google maps key

--- {{cookiecutter.component_id}}/ios/Runner/AppDelegate.swift
+++ {{cookiecutter.component_id}}/ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("{{cookiecutter.google_maps_apikey}}")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
