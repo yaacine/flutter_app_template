80c9359103b1f03286326ef7903c5ff866ad4825
yaacine <gy_zidelmal@esi.dz>
2020-11-18 20:04:18 +0100

    chore: trying the conditional

--- {{cookiecutter.component_id}}/lib/main.dart
+++ {{cookiecutter.component_id}}/lib/main.dart
@@ -4,6 +4,14 @@ void main() {
   runApp(MyApp());
 }
 
+{%- if cookiecutter.license == "MIT" -%}
+# Possible license content here
+
+{%- elif cookiecutter.license == "BSD-3" -%}
+# More possible license content here
+
+{% endif %}
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
